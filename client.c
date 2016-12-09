7b489bea5d089371371f2f1c5249082fde61cf98
Ruochong Wu <rwu3@stuy.edu>
2016-12-09 16:36:19 -0500

    client.c updated

--- client.c
+++ client.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/shm.h>
 #include <sys/ipc.h>
+#include <sys/sem.h>
+#include <string.h>
 #include <unistd.h>
-#include 
+#include <fcntl.h>
+#include <sys/stat.h>
+
+union semun{
+  int val;
+  struct semid_ds *buff;
+  unsigned long *buffer;
+  struct seminfo *_buf;
+};
+
+int tryaccess(){
+  int key = ftok("makefile",22);
+  int semid = semget(key,0,0);
+  union semun su;
+  return semctl(semid, 0, GETVAL, su);
+  // return su.val;
+}
+
+int main(){
+  char * line;
+  printf("semval: %d\n", tryaccess());
+}
--- makefile
+++ makefile
@@ -1,4 +1,5 @@
 semaphone: control.c
 	gcc control.c -g -o control
+	gcc client.c -g -o client
 clean:
 	rm control
